a9d587c56e3c33586c96bf8f673a36faf5dd8040
luantrum27 <hoangtheluan2016@gmail.com>
2023-03-05 02:01:45 +0700

    build: update lib

--- src/screens/MessagesScreen.tsx
+++ src/screens/MessagesScreen.tsx
@@ -1,6 +1,7 @@
 
-import { Box, Heading } from "native-base";
+import { AddIcon, Box, Heading, Icon } from "native-base";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import AntDesign from 'react-native-vector-icons/AntDesign';
 import { RootStackParams } from "src/interfaces/TRootStackParams";
 
 type Props = NativeStackScreenProps<RootStackParams, "Messages">
@@ -9,6 +10,9 @@ export const MessagesScreen: React.FC<Props> = ({navigation}) => {
   return (
     <Box>
       <Heading size={"xs"}>Messages</Heading>
+      <Icon as={AntDesign} name="android1" color="coolGray.800" _dark={{
+        color: "warmGray.50"
+      }} />
     </Box>
   );
 };
